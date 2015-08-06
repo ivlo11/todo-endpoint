5d89747512b70edc3ce4cd4857b5e5afd37ecef4
Ivonne Roberts <ivlo11@gmail.com>
2015-08-05 23:23:10 -0700

    todo id first pass

--- src/com/ivonneroberts/todo/entity/Todo.java
+++ src/com/ivonneroberts/todo/entity/Todo.java
@@ -1,5 +1,20 @@
 package com.ivonneroberts.todo.entity;
 
+/**
+ * This class represents a single todo which has an id, a message
+ * and a completed flag
+ * @author ivonne
+ *
+ */
 public class Todo {
+
+	public void setId(int i) {
+		// TODO Auto-generated method stub
+	}
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
