1b93201137f3ba26ddcb21016c4abce1882811ee
Ivonne Roberts <ivlo11@gmail.com>
2015-08-05 23:02:25 -0700

    initial testing skeleton

--- src-test/com/ivonneroberts/todo/entity/TodoTest.java
+++ src-test/com/ivonneroberts/todo/entity/TodoTest.java
@@ -1,5 +1,16 @@
 package com.ivonneroberts.todo.entity;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class TodoTest {
+	
+	@Test
+	public void testTodoService()
+	{
+		Todo todo = new Todo();
+		assertTrue(todo != null);
+	}
 
 }
--- src-test/com/ivonneroberts/todo/service/TodoServiceApiTest.java
+++ src-test/com/ivonneroberts/todo/service/TodoServiceApiTest.java
@@ -1,5 +1,15 @@
 package com.ivonneroberts.todo.service;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TodoServiceApiTest {
 
+	@Test
+	public void testTodoService()
+	{
+		TodoServiceApi apiTodoService = new TodoServiceApi();
+		assertTrue(apiTodoService != null);
+	}
 }
