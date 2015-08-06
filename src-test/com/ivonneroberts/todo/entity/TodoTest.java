d6cf880add49e1811c09075639ccc84378a92787
Ivonne Roberts <ivlo11@gmail.com>
2015-08-05 23:25:32 -0700

    todo message test

--- src-test/com/ivonneroberts/todo/entity/TodoTest.java
+++ src-test/com/ivonneroberts/todo/entity/TodoTest.java
@@ -12,6 +12,10 @@ public class TodoTest {
 		Todo todo = new Todo();
 		todo.setId(1);
 		assertEquals(1, todo.getId());
+		
+		todo.setMessage("My First Task");
+		assertEquals("My First Task", todo.getMessage());
+
 	}
 
 }
