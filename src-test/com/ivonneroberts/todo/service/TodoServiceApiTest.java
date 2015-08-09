7b4b855eed28175b86c76f8a4c458db438e0c9cf
Ivonne Roberts <ivlo11@gmail.com>
2015-08-08 23:58:27 -0700

    implement basic add, get, update & delete

--- src-test/com/ivonneroberts/todo/service/TodoServiceApiTest.java
+++ src-test/com/ivonneroberts/todo/service/TodoServiceApiTest.java
@@ -2,7 +2,11 @@ package com.ivonneroberts.todo.service;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.ivonneroberts.todo.entity.Todo;
 
 public class TodoServiceApiTest {
 
@@ -11,5 +15,28 @@ public class TodoServiceApiTest {
 	{
 		TodoServiceApi apiTodoService = new TodoServiceApi();
 		assertTrue(apiTodoService != null);
+		
+		Todo todo = apiTodoService.add("My First Task");
+		List<Todo> allTodos = apiTodoService.getAllTodos();
+		
+		assertNotNull(allTodos);
+		assertSame(todo, allTodos.get(0));
+		
+		apiTodoService.setTodoCompleted(0);
+		assertTrue(todo.getCompleted());
+		
+		Todo todo2 = apiTodoService.add("My Second Task");
+		allTodos = apiTodoService.getAllTodos();
+		assertEquals(2, allTodos.size());
+		assertSame(todo2, allTodos.get(1));
+
+		apiTodoService.deleteTodo(0);
+		allTodos = apiTodoService.getAllTodos();
+		assertSame(todo2, allTodos.get(0));
+		
+		/*
+		 * handle sorting alpha desc ascd, custom sequencing
+		 */
+
 	}
 }
--- src/com/ivonneroberts/todo/service/TodoServiceApi.java
+++ src/com/ivonneroberts/todo/service/TodoServiceApi.java
@@ -1,5 +1,44 @@
 package com.ivonneroberts.todo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ivonneroberts.todo.entity.Todo;
+
 public class TodoServiceApi {
+	List<Todo> lstTodos = new ArrayList<Todo>();
+
+	public Todo add(String todoMessage) {
+		Todo todo = new Todo(todoMessage);
+
+		lstTodos.add(todo);
+		return todo;
+	}
+
+	public List<Todo> getAllTodos() {
+		return lstTodos;
+	}
+
+	public Todo setTodoCompleted(int i) {
+		for(Todo todo : lstTodos)
+		{
+			if (todo.getId() == i)
+			{
+				todo.setCompleted(true);
+				return todo;
+			}
+		}
+		return null;
+	}
+
+	public void deleteTodo(int i) {
+		for(Todo todo : lstTodos)
+		{
+			if (todo.getId() == i)
+			{
+				lstTodos.remove(todo);
+			}
+		}
+	}
 
 }
