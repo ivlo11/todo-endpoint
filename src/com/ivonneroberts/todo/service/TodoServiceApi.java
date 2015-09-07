73c64376d790352d367f79b0bd3559bab8083b83
Ivonne Roberts <ivlo11@gmail.com>
2015-09-06 18:08:34 -0700

    add logging to main endpoint

--- src/com/ivonneroberts/todo/service/TodoServiceApi.java
+++ src/com/ivonneroberts/todo/service/TodoServiceApi.java
@@ -1,6 +1,7 @@
 package com.ivonneroberts.todo.service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -14,14 +15,18 @@ import com.ivonneroberts.todo.entity.Todo;
 @Api(name="todo", version="v1", description="An api to manage basic todo")
 public class TodoServiceApi {
 
+	private static final Logger log = Logger.getLogger(TodoServiceApi.class.getName());
+
 	public Todo add(@Named("message") String todoMessage) {
 		Todo todo = new Todo(todoMessage);
 		ofy().save().entity(todo).now();
+		log.info("Saved todo: " + todo.getId());
 		return todo;
 	}
 
 	@ApiMethod(path = "todos")
 	public List<Todo> getAllTodos() {
+		log.info("Getting all todos... ");
 		return ofy()
 		          .load()
 		          .type(Todo.class)
@@ -34,6 +39,7 @@ public class TodoServiceApi {
 		Todo todo = _getTodoEntity(id);
 		todo.setCompleted(Boolean.TRUE);
 		ofy().save().entity(todo).now();
+		log.info("Completed todo: " + todo.getId());
 		return todo;
 	}
 
@@ -47,6 +53,7 @@ public class TodoServiceApi {
 		Todo todo = ofy().load().type(Todo.class).id(id).now();
 		if (todo == null)
 		{
+			log.severe("Todo " + id + " does not exist!");
 			throw new NotFoundException("Todo does not exist");
 		}
 		return todo;
